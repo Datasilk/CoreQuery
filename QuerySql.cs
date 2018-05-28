571719b1cd21d8a6a1867d2812b3fac0f4350d63
Mark Entingh <entingh@gmail.com>
2018-05-28 11:35:40 -0700

    added initial classes

---
+++ QuerySql.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Query
+{
+    public abstract class QuerySql
+    {
+        public Sql Sql;
+        public static string connectionString;
+
+        public QuerySql()
+        {
+            Sql = new Sql(connectionString);
+        }
+
+    }
+}
