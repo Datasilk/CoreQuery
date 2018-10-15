a21f74ae81d237ffa7d97d2948a85cb3e35da21f
Mark Entingh <entingh@gmail.com>
2018-10-15 15:35:29 -0700

    changed Core Query to be completely static

--- Sql.cs
+++ Sql.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
@@ -7,37 +6,11 @@ using Dapper;
 
 namespace Query
 {
-    public class Sql
+    public static class Sql
     {
-        private SqlConnection conn = new SqlConnection();
-        private SqlCommand cmd = new SqlCommand();
-        private string connString = "";
+        public static string connectionString;
 
-        public Sql(string connectionString)
-        {
-            connString = connectionString;
-        }
-
-
-        private void Start()
-        {
-            if (conn.State == System.Data.ConnectionState.Closed)
-            {
-                conn.ConnectionString = connString;
-                conn.Open();
-                cmd.Connection = conn;
-            }
-        }
-
-        public void Close()
-        {
-            if (conn.State != System.Data.ConnectionState.Closed)
-            {
-                conn.Close();
-            }
-        }
-
-        private string GetStoredProc(string storedproc, Dictionary<string, object> parameters = null)
+        private static string GetStoredProc(string storedproc, Dictionary<string, object> parameters = null)
         {
             var sql = new StringBuilder("EXEC " + storedproc);
             if (parameters != null)
@@ -52,7 +25,7 @@ namespace Query
             return sql.ToString();
         }
 
-        private List<SqlParameter> GetSqlParameters(Dictionary<string, object> parameters = null)
+        private static List<SqlParameter> GetSqlParameters(Dictionary<string, object> parameters = null)
         {
             var parms = new List<SqlParameter>();
             foreach (var parm in parameters)
@@ -62,91 +35,76 @@ namespace Query
             return parms;
         }
 
-        public SqlDataReader ExecuteReader(string storedproc, Dictionary<string, object> parameters = null)
+        public static SqlDataReader ExecuteReader(string storedproc, Dictionary<string, object> parameters = null)
         {
-            Start();
-            cmd.Parameters.Clear();
-            cmd.CommandText = GetStoredProc(storedproc, parameters);
-            try
+            using (var conn = new SqlConnection(connectionString))
             {
-                if (parameters != null) { GetSqlParameters(parameters).ForEach(a => cmd.Parameters.Add(a)); }
-                return cmd.ExecuteReader();  //developer must manually close connection within connection pool
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void ExecuteNonQuery(string storedproc, Dictionary<string, object> parameters = null)
-        {
-            Start();
-            cmd.Parameters.Clear();
-            cmd.CommandText = GetStoredProc(storedproc, parameters);
-            try
-            {
-                if (parameters != null) { GetSqlParameters(parameters).ForEach(a => cmd.Parameters.Add(a)); }
-                cmd.ExecuteNonQuery();
-                Close(); //make sure to close connection within connection pool
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public T ExecuteScalar<T>(string storedproc, Dictionary<string, object> parameters = null)
-        {
-            Start();
-            cmd.Parameters.Clear();
-            cmd.CommandText = GetStoredProc(storedproc, parameters);
-            try
-            {
-                if (parameters != null) { cmd.Parameters.AddRange(GetSqlParameters(parameters).ToArray()); }
-                var result = (T)cmd.ExecuteScalar();
-                Close(); //make sure to close connection within connection pool
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public async Task<int> ExecuteNonQueryAsync(string storedproc, Dictionary<string, object> parameters = null)
-        {
-            using (var newConnection = new SqlConnection(connString))
-            {
-                using (var newCommand = new SqlCommand(GetStoredProc(storedproc, parameters), newConnection))
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
                 {
-                    try
-                    {
-                        if (parameters != null) newCommand.Parameters.AddRange(GetSqlParameters(parameters).ToArray());
-                        await newConnection.OpenAsync().ConfigureAwait(false);
-                        return await newCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
-
-                    }
-                    catch (Exception ex)
-                    {
-                        throw ex;
-                    }
+                    conn.Open();
+                    return cmd.ExecuteReader();
                 }
             }
         }
 
-        public List<T> Populate<T>(string storedproc, Dictionary<string, object> parameters = null)
+        public static void ExecuteNonQuery(string storedproc, Dictionary<string, object> parameters = null)
         {
-            Start();
-            var results = conn.Query<T>(GetStoredProc(storedproc, parameters), parameters).AsList<T>();
-            Close(); //make sure to close connection within connection pool
-            return results;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
-        public SqlMapper.GridReader PopulateMultiple(string storedproc, Dictionary<string, object> parameters = null)
+        public static T ExecuteScalar<T>(string storedproc, Dictionary<string, object> parameters = null)
         {
-            Start();
-            //developer must manually close connection within connection pool
-            return conn.QueryMultiple(GetStoredProc(storedproc, parameters), parameters);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
+                {
+                    conn.Open();
+                    return (T)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static async Task<int> ExecuteNonQueryAsync(string storedproc, Dictionary<string, object> parameters = null)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
+                {
+                    await conn.OpenAsync().ConfigureAwait(false);
+                    return await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        public static List<T> Populate<T>(string storedproc, Dictionary<string, object> parameters = null)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
+                {
+                    conn.Open();
+                    return conn.Query<T>(GetStoredProc(storedproc, parameters), parameters).AsList<T>();
+                }
+            }
+        }
+
+        public static SqlMapper.GridReader PopulateMultiple(string storedproc, Dictionary<string, object> parameters = null)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(GetStoredProc(storedproc, parameters), conn))
+                {
+                    conn.Open();
+                    return conn.QueryMultiple(GetStoredProc(storedproc, parameters), parameters);
+                }
+            }
         }
     }
 }
